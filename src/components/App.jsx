bd55672af5983eab451a0fd3bcb85962fea6181a
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-01-17 23:59:35 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,7 @@ import { Statistics } from "./Statistics/Statistics";
 import { FriendList } from "./FriendList/FriendList";
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
 
-// import username from "./data/user";
-// import tag from "./data/user";
-// import location from "./data/user";
-// import avatar from "./data/user";
-// import stats from "./data/user";
+
 import user from './data/user';
 import data from './data/data';
 import friends from './data/friends';
