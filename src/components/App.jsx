b08bae02a5f6546faff61921dcd339c7a400c590
irnikshu <95768966+irnikshu@users.noreply.github.com>
2023-01-17 21:08:47 +0200

    add Profile, Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
+import { Profile } from "./Profile/Profile";
+import { Statistics } from "./Statistics/Statistics";
+import { FriendList } from "./FriendList/FriendList";
+import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+
+// import username from "./data/user";
+// import tag from "./data/user";
+// import location from "./data/user";
+// import avatar from "./data/user";
+// import stats from "./data/user";
+import user from './data/user';
+import data from './data/data';
+
+
+
+
 export const App = () => {
+  // return (
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
+  // );
+   const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
+    <Statistics title="Upload stats" stats={data} />
+    </>
   );
 };
